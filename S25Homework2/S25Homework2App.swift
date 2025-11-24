808e836f2d303f629f2953e2e0517b6d6e48e6d3
KangDaHeee <dhk3161@gmail.com>
2025-11-24 16:50:10 +0900

    Initial Commit

---
+++ S25Homework2/S25Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  S25Homework2App.swift
+//  S25Homework2
+//
+//  Created by Student on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct S25Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ S25Homework2Tests/S25Homework2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  S25Homework2Tests.swift
+//  S25Homework2Tests
+//
+//  Created by Student on 11/24/25.
+//
+
+import Testing
+@testable import S25Homework2
+
+struct S25Homework2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
